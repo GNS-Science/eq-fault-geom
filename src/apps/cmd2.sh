d9de419a1333a99e99f8f1ca3a4c2dfabd0022cc
Charles Williams <C.Williams at gns.cri.nz>
2020-07-20 18:47:07 +1200

    Removed early testing files.

--- src/apps/cmd2.sh
+++
@@ -1,1 +0,0 @@
-./tsurf2vtk.py --in_files=/Users/charlesw/GNS-svn/charlesw/active-faults/hannu/NZ-CFM-V0.3-June-2020/Tsurf_individual_fault_files --out_files=/Users/charlesw/GNS-svn/eq-fault-geom/test2
