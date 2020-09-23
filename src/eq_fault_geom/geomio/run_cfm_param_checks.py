0726713bcdc6c0536f9f5ff6d16797b233732378
arh79 <arh79@cam.ac.uk>
2020-09-23 13:02:13 +1200

    Change dip direction convention

--- src/eq_fault_geom/geomio/run_cfm_param_checks.py
+++ src/eq_fault_geom/geomio/run_cfm_param_checks.py
@@ -3,6 +3,6 @@ from eq_fault_geom.geomio.cfm_faults import CfmMultiFault
 shp = "/Users/arh79/PycharmProjects/eq-fault-geom/src/eq_fault_geom/geomio/cfm_linework/NZ_CFM_v0_3_170620.shp"
 
 data = CfmMultiFault.from_shp(shp)
-xml = data.to_opensha_xml(exclude_subduction=True)
+xml = data.to_opensha_xml(exclude_subduction=False)
 with open("test3.xml", "wb") as f:
     f.write(xml)
