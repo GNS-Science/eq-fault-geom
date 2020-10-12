a1c7c90d60bda345f0a6161788c43eb9a87170d9
arh79 <arh79@cam.ac.uk>
2020-10-13 10:00:52 +1300

    Combine Andy's BOP polygon with Matt's TVZ polygon

--- src/eq_fault_geom/geomio/run_cfm_param_checks.py
+++ src/eq_fault_geom/geomio/run_cfm_param_checks.py
@@ -9,10 +9,11 @@ shp = "/Users/arh79/PycharmProjects/eq-fault-geom/src/eq_fault_geom/geomio/cfm_l
 # Polygons to exclude faults from XML
 exclude_shp = "/Users/arh79/PycharmProjects/eq-fault-geom/src/eq_fault_geom/geomio/test_bop.shp"
 exclude_df = gpd.GeoDataFrame.from_file(exclude_shp)
-poly_ls = list(exclude_df.geometry)
+# Change to WGS for use with Matt's TVZ polygon
+exclude_df_wgs = exclude_df.to_crs(epsg=4326)
+poly_ls = list(exclude_df_wgs.geometry)
 
-# # To read in Matt's polygon
-
+# To read in Matt's TVZ polygon
 matt_array = np.array([[-36.17, 177.25],
                        [-36.17, 178.14],
                        [-37.53, 177.31],
@@ -21,10 +22,15 @@ matt_array = np.array([[-36.17, 177.25],
                        [-39.22, 175.29],
                        [-36.17, 177.25]])
 
+# Polygon requires lon lat (rather than lat lon)
 matt_poly = [Polygon([(row[1], row[0]) for row in matt_array])]
-matt_gs = gpd.GeoSeries(matt_poly[0], crs=4326)
-matt_gs.to_file("matt_poly.shp")
+poly_ls.append(matt_poly)
 
+# # Write to shapefile for visualization in GIS
+# matt_gs = gpd.GeoSeries(matt_poly[0], crs=4326)
+# matt_gs.to_file("matt_poly.shp")
+
+# Read and write data
 data = CfmMultiFault.from_shp(shp, exclude_regions=poly_ls)
 xml = data.to_opensha_xml(exclude_subduction=True)
 with open("test3.xml", "wb") as f:
