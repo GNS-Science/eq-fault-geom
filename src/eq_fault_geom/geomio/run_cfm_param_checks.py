75b145eddeadddc1997b89788371dec0fd3f5897
arh79 <arh79@cam.ac.uk>
2020-10-13 09:44:52 +1300

    Includes new polygon

--- src/eq_fault_geom/geomio/run_cfm_param_checks.py
+++ src/eq_fault_geom/geomio/run_cfm_param_checks.py
@@ -1,7 +1,8 @@
-from eq_fault_geom.geomio.cfm_faults import CfmMultiFault
 import geopandas as gpd
-from shapely.geometry import Polygon
 import numpy as np
+from shapely.geometry import Polygon
+
+from eq_fault_geom.geomio.cfm_faults import CfmMultiFault
 
 shp = "/Users/arh79/PycharmProjects/eq-fault-geom/src/eq_fault_geom/geomio/cfm_linework/NZ_CFM_v0_3_170620.shp"
 
@@ -11,15 +12,18 @@ exclude_df = gpd.GeoDataFrame.from_file(exclude_shp)
 poly_ls = list(exclude_df.geometry)
 
 # # To read in Matt's polygon
-# matt_array = np.array([[-37.65, 178.79],
-#                        [-39.22, 178.79],
-#                        [-41.001,  176.77],
-#                        [-41.001, 175.31],
-#                        [-40.17,  176.23],
-#                        [-37.65,  178.79]])
-# matt_poly = [Polygon([(row[1], row[0]) for row in matt_array])]
-# matt_gs = gpd.GeoSeries(matt_poly[0], crs=4326)
-# matt_gs.to_file("matt_poly.shp")
+
+matt_array = np.array([[-36.17, 177.25],
+                       [-36.17, 178.14],
+                       [-37.53, 177.31],
+                       [-39.78, 175.38],
+                       [-39.78, 174.97],
+                       [-39.22, 175.29],
+                       [-36.17, 177.25]])
+
+matt_poly = [Polygon([(row[1], row[0]) for row in matt_array])]
+matt_gs = gpd.GeoSeries(matt_poly[0], crs=4326)
+matt_gs.to_file("matt_poly.shp")
 
 data = CfmMultiFault.from_shp(shp, exclude_regions=poly_ls)
 xml = data.to_opensha_xml(exclude_subduction=True)
