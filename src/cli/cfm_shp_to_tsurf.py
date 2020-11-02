6281387f29b16a30d2a14a703e41d856b5b3b564
Charles Williams <C.Williams at gns.cri.nz>
2020-11-02 16:29:28 +1300

    Changed scripts to accept command-line arguments and moved them to new cli directory.

---
+++ src/cli/cfm_shp_to_tsurf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Simple script to convert entries from the current CFM shapefile into
+multiple tsurf files.
+"""
+if __name__ == "__main__":
+    from pathlib import Path
+    import argparse
+    import geopandas as gpd
+    import pandas as pd
+    import numpy as np
+    import meshio
+    from eq_fault_geom.faultmeshops.trace_to_mesh import project_mesh_from_trace
+    import eq_fault_geom.faultmeshio.tsurf as ts
+    # import pdb
+    # pdb.set_trace()
+
+    parser = argparse.ArgumentParser(description='Convert files from the CFM shapefile to Tsurf format.')
+    parser.add_argument("-i", "--in_file", action="store", 
+                        dest="in_file", required=True, help="input shapefile")
+    parser.add_argument("-o", "--out_dir", action="store", 
+                        dest="out_dir", required=True, help="output directory")
+
+    args = parser.parse_args()
+
+    in_path = Path(args.in_file)
+    out_path = Path(args.out_dir)
+    out_path.mkdir(parents=True, exist_ok=True)
+
+    # File suffix.
+    tsurf_suffix = ".ts"
+
+    # Cell type.
+    cell_type = 'triangle'
+
+    # read in data
+    shp_df = gpd.GeoDataFrame.from_file(in_path)
+
+    # Sort alphabetically by name
+    sorted_df = shp_df.sort_values("Name")
+
+    # Reset index to line up with alphabetical sorting
+    sorted_df = sorted_df.reset_index(drop=True)
+
+    # Loop through faults, creating a Tsurf file for each.
+    for i, fault in sorted_df.iterrows():
+        # Create fault segment by projecting mesh downdip.
+        faultmesh = project_mesh_from_trace(fault, cell_type=cell_type, cell_fields=None)
+
+        # Get fault name.
+        fault_name = fault["FZ_Name"].replace(" ", "_")
+
+        # Create Tsurf mesh.
+        x = faultmesh.points[:,0]
+        y = faultmesh.points[:,1]
+        z = faultmesh.points[:,2]
+        cells = faultmesh.cells
+        tsurf_mesh = ts(x, y, z, cells, name=fault_name)
+
+        # Write Tsurf mesh.
+        file_path = Path(fault_name)
+        output_file = Path.joinpath(out_path, file_path).with_suffix(tsurf_suffix)
+        tsurf_mesh.write(output_file)
+
---
+++ src/cli/test_tsurf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ./cfm_shp_to_tsurf.py --in_file=../../data/cfm_shapefile/cfm_lower_n_island.shp --out_dir=out_tsurf
