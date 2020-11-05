fe30f818feb4ebfb9d1670ed9f863885d8a6317f
Charles Williams <C.Williams at gns.cri.nz>
2020-11-05 14:09:09 +1300

    Fixed import.

--- src/eq_fault_geom/faultmeshops/trace_to_mesh.py
+++ src/eq_fault_geom/faultmeshops/trace_to_mesh.py
@@ -7,7 +7,7 @@ import numpy as np
 
 import meshio
 
-from trace_operations import calculate_dip_rotation
+from .trace_operations import calculate_dip_rotation
 
 
 def project_mesh_from_trace(fault_info: pd.Series, cell_type: str = "triangle",
